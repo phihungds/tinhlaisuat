2dcc930bd3a3df9cf38e4243252a6426cf1427ba
Phi Hung <phihunds1@gmail.com>
2020-10-12 18:38:07 +0700

    hoan thien

--- laisuat.js
+++ laisuat.js
@@ -1,9 +1,9 @@
 function cal() {
-    let money = document.getElementById("money").value
-    let lai =document.getElementById("lai").value
-    let year = document.getElementById("year").value
+    let money = Number(document.getElementById("money").value)
+    let lai = Number(document.getElementById("lai").value)
+    let year = Number(document.getElementById("year").value)
     let namdau = money * (lai / 100)
     let tientra = money + year * namdau
-    let ketqua = "Sau " + year + " năm tổng số tiền phải trả cả gốc và lãi là : " + tientra
+    let ketqua = "Sau " + year + " năm tổng số tiền phải trả cả gốc và lãi là : " + tientra + " triệu đồng"
     document.getElementById("ketqua").innerText = ketqua
 }
